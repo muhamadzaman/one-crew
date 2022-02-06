38c765b87bdaa24b99b181476ec62bbe4ae7b18c
Muhammad Zamaan <zamandev.us@gmail.com>
2022-02-06 11:08:47 -0800

    Created User and role models and setup user registration and signin

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }, allow_blank: true
+end
